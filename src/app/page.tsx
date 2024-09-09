aa5599bac6d0aa1307591b99f81b1106961042b4
elsigh <elsigh@gmail.com>
2024-09-09 10:26:45 -0700

    newsletter back in action

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import Image from "next/image";
 import Link from "next/link";
-import { redirect } from "next/navigation";
 
 import { Button } from "@/components/Button";
 import { Container } from "@/components/Container";
@@ -15,6 +14,7 @@ import {
   SoundcloudIcon,
   InstagramIcon,
 } from "@/components/SocialIcons";
+import Newsletter from "@/components/Newsletter";
 
 export default async function Home() {
   return (
@@ -32,7 +32,7 @@ export default async function Home() {
 
               <div className="mt-4 flex gap-6 items-end">
                 <SocialLink
-                  href="#"
+                  href="https://www.instagram.com/commonerband"
                   aria-label="Follow on Instagram"
                   icon={InstagramIcon}
                 />
@@ -81,6 +81,7 @@ export default async function Home() {
                   <div>
                     <Link href="/to-seek-or-to-hide">
                       <Image
+                        priority
                         src={SeekHideCoverFront}
                         alt=""
                         sizes="(min-width: 1024px) 32rem, 20rem"
@@ -121,36 +122,12 @@ export default async function Home() {
               </ol>
             </div>
           </div>
-          {/* 
           <div className="space-y-10 lg:pr-52">
             <Newsletter />
-          </div>*/}
+          </div>
         </div>
       </Container>
     </>
-  );
-}
-
-function MailIcon(props: React.ComponentPropsWithoutRef<"svg">) {
-  return (
-    <svg
-      viewBox="0 0 24 24"
-      fill="none"
-      strokeWidth="1.5"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      aria-hidden="true"
-      {...props}
-    >
-      <path
-        d="M2.75 7.75a3 3 0 0 1 3-3h12.5a3 3 0 0 1 3 3v8.5a3 3 0 0 1-3 3H5.75a3 3 0 0 1-3-3v-8.5Z"
-        className="fill-zinc-100 stroke-zinc-400 dark:fill-zinc-100/10 dark:stroke-zinc-500"
-      />
-      <path
-        d="m4 6 6.024 5.479a2.915 2.915 0 0 0 3.952 0L20 6"
-        className="stroke-zinc-400 dark:stroke-zinc-500"
-      />
-    </svg>
   );
 }
 
@@ -169,72 +146,5 @@ function SocialLink({
     >
       <Icon className="h-6 w-6 fill-zinc-500 transition group-hover:fill-zinc-600 dark:fill-zinc-400 dark:group-hover:fill-zinc-300" />
     </Link>
-  );
-}
-
-export async function submitEmail(formData: FormData) {
-  const email = formData.get("email") as string;
-
-  try {
-    const response = await fetch(
-      "https://docs.google.com/forms/d/e/1FAIpQLSd02hwbAMI9JjX4vgmwveHWiM8xOIccYOI_N4V73-ksb1a_ow/formResponse",
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/x-www-form-urlencoded",
-        },
-        body: new URLSearchParams({
-          "entry.1905632543": email,
-          email,
-        }),
-      },
-    );
-
-    return response;
-  } catch (error) {
-    console.error("Error submitting email:", error);
-    throw new Error("Error submitting email.");
-  }
-}
-
-function Newsletter() {
-  const handleSubmit = async (formData: FormData) => {
-    "use server";
-    try {
-      const response = await submitEmail(formData);
-      if (response.ok) {
-        redirect("/thank-you");
-      } else {
-        redirect("/error");
-      }
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  return (
-    <form
-      action={handleSubmit}
-      className="rounded-2xl border border-zinc-100 p-6 dark:border-zinc-700/40"
-    >
-      <h2 className="flex text-sm font-semibold text-zinc-100">
-        <MailIcon className="h-6 w-6 flex-none" />
-        <span className="ml-3">Stay up to date</span>
-      </h2>
-      <p className="mt-2 text-sm text-zinc-400">Find out when we're playing.</p>
-      <div className="mt-6 flex">
-        <input
-          type="email"
-          placeholder="Email"
-          aria-label="Email address"
-          required
-          autoComplete="off"
-          className="min-w-0 flex-auto appearance-none rounded-md border border-zinc-900/10 bg-white px-3 py-[calc(theme(spacing.2)-1px)] shadow-md shadow-zinc-800/5 placeholder:text-zinc-400 focus:border-orange-900 focus:outline-none focus:ring-4 focus:ring-orange-700/10 sm:text-sm dark:border-zinc-700 dark:bg-zinc-700/[0.15] dark:text-zinc-200 dark:placeholder:text-zinc-500 dark:focus:border-orange-700 dark:focus:ring-orange-500/10"
-        />
-        <Button type="submit" className="ml-4 flex-none">
-          Join
-        </Button>
-      </div>
-    </form>
   );
 }
