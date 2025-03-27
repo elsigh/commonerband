dc52fc304b28612bb6dc131dd9ac2c65d3802df4
elsigh <elsigh@gmail.com>
2025-03-27 14:29:52 -0700

    lil fix

---
+++ src/components/EmailTemplate.jsx
@@ -0,0 +1,10 @@
+export function EmailTemplate({ email }) {
+  return (
+    <div>
+      <h1>Contact from commoner.com</h1>
+      <p>
+        <b>Email</b>: {email}
+      </p>
+    </div>
+  );
+}
